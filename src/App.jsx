f33862f2f990e81e9cc5098a2b7a0a7b9a6264e1
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-08 16:33:26 -0800

    initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,60 @@
+import { useState } from 'react';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState('Bob Loblaw');
+  const [entry, setEntry] = useState('');
+  const [entries, setEntries] = useState([
+    { id: 'Jojo-0', name: 'Jojo', comment: 'hey it is me' },
+  ]);
+
+  const handleChangeUser = (e) => {
+    e.preventDefault();
+    setCurrentUser('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEntries([...entries, { entry }]);
+    setCurrentUser(user);
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>Guestbook</h1>
+        <h2>Howdy, {user}</h2>
+      </header>
+      <main>
+        <form>
+          {currentUser ? (
+            <>
+              <span>Not {currentUser}? </span>
+              <button onClick={handleChangeUser}>Change user</button>
+            </>
+          ) : (
+            <>
+              <div className="name">
+                <span>Guest Name:</span>
+                <input value={user} onChange={(e) => setUser(e.target.value)} />
+              </div>
+            </>
+          )}
+          <div className="entry">
+            <span>Guest Entry:</span>
+            <input value={entry} onChange={(e) => setEntry(e.target.value)} />
+          </div>
+          <button onClick={handleSubmit}>Sign</button>
+        </form>
+        {entries.length > 0
+          ? entries.map((entry) => (
+              <div key={entry.id}>
+                <h3>{entry.name}</h3>
+                <p>{entry.comment}</p>
+              </div>
+            ))
+          : null}
+      </main>
+    </div>
+  );
 }
