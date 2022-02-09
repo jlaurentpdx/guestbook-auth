390fbc73ace64b17797fd6254124a30f94f98be3
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-08 17:30:39 -0800

    refactored file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,12 @@
-import { useState } from 'react';
+import Layout from './views/Layout/Layout';
+import Home from './views/Home/Home';
 
 export default function App() {
-  const [user, setUser] = useState('');
-  const [currentUser, setCurrentUser] = useState('');
-  const [entry, setEntry] = useState('');
-  const [entries, setEntries] = useState([]);
-
-  const handleChangeUser = (e) => {
-    e.preventDefault();
-    setCurrentUser('');
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = new Date();
-    setCurrentUser(user);
-
-    const newEntry = { id: id, name: user, comment: entry };
-
-    setEntries([...entries, newEntry]);
-  };
-
   return (
-    <div>
-      <header>
-        <h1>Guestbook</h1>
-      </header>
-      <main>
-        <form>
-          {currentUser ? (
-            <>
-              <h2>Howdy, {currentUser}</h2>
-              <span>Not {currentUser}? </span>
-              <button onClick={handleChangeUser}>Change user</button>
-            </>
-          ) : (
-            <>
-              <div className="name">
-                <span>Guest Name:</span>
-                <input value={user} onChange={(e) => setUser(e.target.value)} />
-              </div>
-            </>
-          )}
-          <div className="entry">
-            <span>Guest Entry:</span>
-            <input value={entry} onChange={(e) => setEntry(e.target.value)} />
-          </div>
-          <button onClick={handleSubmit}>Sign</button>
-        </form>
-        {entries.length > 0
-          ? entries.map((entry) => (
-              <div key={entry.id}>
-                <h3>{entry.name}</h3>
-                <p>{entry.comment}</p>
-              </div>
-            ))
-          : null}
-      </main>
+    <div className="App">
+      <Layout>
+        <Home />
+      </Layout>
     </div>
   );
 }
